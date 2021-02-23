d3ba6103ed0328bde0453f6dfc155ec6c7d49b15
Alwyn Kik <alwyn@kik.pw>
2021-02-24 00:48:15 +0100

    Rename app

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="potatodemo",
+    name="potato",
     version="0.1",
     description="Potato demo application.",
     packages=find_packages(),
