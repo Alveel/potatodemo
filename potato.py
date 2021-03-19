862d348b16c0df482563f976402a8c091a15a1ca
Alwyn Kik <alwyn@kik.pw>
2021-03-19 16:30:00 +0100

    Add azure vault test

--- potato.py
+++ potato.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask, render_template
 from markupsafe import escape
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
 
 app = Flask(__name__)
 
@@ -35,6 +39,26 @@ def potato():
 @app.route('/tomato')
 def tomato():
     return render_template('tomato.html')
+
+
+@app.route('/vault')
+def vault():
+    key_vault_name = os.getenv("KEY_VAULT_NAME", "tst-ocp2-temp")
+
+    kv_uri = f"https://{key_vault_name}.vault.azure.net"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=kv_uri, credential=credential)
+    secret_name = "secret"
+    # secret_name = input("Input a name for your secret > ")
+    # secretValue = input("Input a value for your secret > ")
+    # print(f"Creating a secret in {key_vault_name} called '{secret_name}' with the value '{secretValue}' ...")
+    # client.set_secret(secret_name, secretValue)
+    # print(f"Retrieving your secret from {key_vault_name}.")
+    retrieved_secret = client.get_secret(secret_name)
+    # print(f"Deleting your secret from {key_vault_name} ...")
+    # poller = client.begin_delete_secret(secret_name)
+    # deleted_secret = poller.result()
+    return f"Your secret is '{retrieved_secret.value}'."
 
 
 if __name__ == '__main__':
